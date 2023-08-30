b6183dc3d8b1262ef85fbe14d2bc905c1b76f0be
Dušan Mitrović <dusan@dusanmitrovic.xyz>
2023-08-31 01:19:02 +0200

    Escaped special characters
    Signed-off-by: Dušan Mitrović <dusan@dusanmitrovic.xyz>

--- pkg/wifi/wifi.go
+++ pkg/wifi/wifi.go
@@ -1,6 +1,7 @@
 package wifi
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strconv"
 )
@@ -48,10 +49,44 @@ func New(
 		return WiFi{}, fmt.Errorf("ssid cannot be empty")
 	}
 
+	ssid = escapeHexString(escapeSpecialCharacters(ssid))
+	password = escapeHexString(escapeSpecialCharacters(password))
+
 	return WiFi{
 		Security: security,
 		SSID:     ssid,
 		Password: password,
 		Hidden:   hidden,
 	}, nil
+}
+
+func escapeHexString(input string) string {
+	if len(input)%2 != 0 {
+		return input
+	}
+
+	if _, err := hex.DecodeString(input); err == nil {
+		// The string can be interpreted as a hex number
+		// therefore we wrap it in double quotes
+		input = fmt.Sprintf("\"%s\"", input)
+	}
+
+	return input
+}
+
+func escapeSpecialCharacters(input string) string {
+	runes := make([]rune, 0, len(input))
+	for _, c := range input {
+		if c == '\\' ||
+			c == ';' ||
+			c == ',' ||
+			c == '"' ||
+			c == ':' {
+			runes = append(runes, '\\')
+		}
+
+		runes = append(runes, c)
+	}
+
+	return string(runes)
 }
